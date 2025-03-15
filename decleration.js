1524bff453d35eecc2d8bac3a289712ac5d5b956
Prasanjeet Panwar <ppanwar@cholachariots.com>
2025-03-15 11:14:01 +0530

    decleartion

---
+++ decleration.js
@@ -0,0 +1,9 @@
+var a;   // this works will give us a undefined value in the console
+console.log(a)
+
+let b;  // this can also works will give us a undefined value in the console
+console.log(b)
+
+
+const c; /// this will throw an error because we are trying to declare a constant
+console.log(c)
