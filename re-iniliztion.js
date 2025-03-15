249c3c7308c610dda7e3562674b1f290121af4f4
Prasanjeet Panwar <ppanwar@cholachariots.com>
2025-03-15 11:16:30 +0530

    re-inilition

---
+++ re-iniliztion.js
@@ -0,0 +1,13 @@
+let a =4
+a=9
+console.log(a)  // this will works for the let keyword
+
+
+var b =4
+b=9
+console.log(b)  // this will also works for the var keyword
+
+
+const c =4
+c=9  // this will throw an error for the const keyword
+console.log(c)  // this will works for the const keyword
